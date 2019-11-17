c7ef1ece60dbd68ada4308a3cffcc71d5c67bce0
lyubomyr83 <lyubomyr83@mail.ru>
2019-11-17 12:00:39 +0300

    rojer

--- Rojer.py
+++ Rojer.py
@@ -1,35 +1,45 @@
+from random import randint, choice
+
 print ('Привет! Меня зовут Роджер. А как тебя?')
 name = input()
 name = name.title()
 print ('Приятно познакомиться,' + name)
-print ('''Давай проверим твои знания в математике.
-Ты готов? (да или нет)''')
-ready = input()
-ready = ready.lower()
+print ('Давай проверим твои знания в математике.')
 
-while ready not in {'да','нет'}:
-    print('''Должно быть да или нет.
-Введи заново.''')
-    ready = input()
-    ready = ready.lower()
+print ('Хорошо')
 
-if ready == 'да':
-    print ('Хорошо')
-
-    print(f'{name}, сколько примеров ты готов решить?')
+print(f'{name}, сколько примеров ты готов решить?')
+questions_guantity = input()
+while not questions_guantity.isdigit():
+    print('Должна быть цифра!')
     questions_guantity = input()
-    while not questions_guantity.isdigit():
-        print('Должна быть цифра!')
-        questions_guantity = input()
 
-    print(f'{name}, сколько примеров ты готов решить?')
+print(f'{name}, до скольки бьудем считать?')
+max_answer = input()
+while not max_answer.isdigit():
+    print('Должна быть цифра!')
     max_answer = input()
-    while not max_answer.isdigit():
-        print('Должна быть цифра!')
-        max_answer = input()
+
+print('Хорошо, тогда начинаем...')
+
+for x in range(int(questions_guantity)):
+    print(f'Пример {x+1}')
+
+    number1 = randint(1, int(max_answer))
+    number2 = randint(1, int(max_answer))
+    sigh = choice('+-')
+
+    if sigh == '-':
+        while number1 < number2:
+            number1 = randint(1, int(max_answer))
+            number2 = randint(1, int(max_answer))
+
+    if sigh == '+':
+        while number1 + number2 > int(max_answer):
+            number1 = randint(1, int(max_answer))
+            number2 = randint(1, int(max_answer))
+
+    print(f'Сколько будет {number1} {sigh} {number2}?')
 
 
 
-else:
-    print ('''Передумал? Хорошо, может как-нибудь в следующий раз...
-Пока!''')
