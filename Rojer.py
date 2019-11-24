e282b2820227cc99ce898120d7394efbe961d247
lyubomyr83 <lyubomyr83@mail.ru>
2019-11-24 11:58:42 +0300

    rojer

--- Rojer.py
+++ Rojer.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+from timeit import default_timer
 
 print ('Привет! Меня зовут Роджер. А как тебя?')
 name = input()
@@ -22,6 +23,11 @@ while not max_answer.isdigit():
 
 print('Хорошо, тогда начинаем...')
 
+
+correct_answers = 0
+fails = 0
+spent_time = 0
+
 for x in range(int(questions_guantity)):
     print(f'Пример {x+1}')
 
@@ -33,13 +39,36 @@ for x in range(int(questions_guantity)):
         while number1 < number2:
             number1 = randint(1, int(max_answer))
             number2 = randint(1, int(max_answer))
+        correct_answer = number1 - number2
 
     if sigh == '+':
         while number1 + number2 > int(max_answer):
             number1 = randint(1, int(max_answer))
             number2 = randint(1, int(max_answer))
+        correct_answer = number1 + number2
 
     print(f'Сколько будет {number1} {sigh} {number2}?')
+    start = default_timer()
+    answer = input('Введи ответ: \n')
+    stop = default_timer()
+    spent_time += round(stop - start)
+
+    if int(answer) == correct_answer:
+        print('Правильно!')
+        correct_answers += 1
+    else:
+        print(f'Неправильно. Правильный ответ: {correct_answer}')
+        fails += 1
+
+
+if fails != 0:
+    print(f'''
+Правильных ответов: {correct_answers}
+Ошибок: {fails}
+Затраченное время: {spent_time}''')
+else:
+    print(f'Ты ответил правильно на все вопросы за: {spent_time}!')
+
 
 
 
