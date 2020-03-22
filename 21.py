e65f6ce96aa151a0e95b820d1d00868532be5e58
lyubomyr83 <lyubomyr83@mail.ru>
2020-03-22 12:10:29 +0300

    22032020

--- 21.py
+++ 21.py
@@ -1,7 +1,37 @@
 from random import shuffle
 
-koloda = [2,3,4,5,6,7,8,9,10,10,10,10,11]*4
-shuffle(koloda)
+
+#функция возвращает перемешанную колоду
+def get_deck():
+    deck = []
+
+    for suit in ('черви','пики','бубны','трефы'):
+        for card in range(2,11):
+            deck.append(f'{card} {suit}')
+        for card in ('валет', 'дама', 'король', 'туз'):
+            deck.append(f'{card} {suit}')
+    shuffle(deck)
+    return deck
+
+
+#функция возвращает определённое количество очков для карты
+def get_card_points(given_card):
+    card_name = given_card.split()
+
+    card_points = {}
+    for card in range(2,11):
+        card_points[f'{card}'] = card
+    for card in ('валет', 'дама', 'король'):
+        card_points[f'{card}'] = 10
+    if card_name[0] == 'туз':
+        points = input('туз 1 или 11?\n')
+    else:
+        points = card_points[card_name[0]]
+
+
+    return points
+
+deck = get_deck()
 
 your_points = 0
 computer_points = 0
@@ -11,18 +41,19 @@ more = 'да'
 
 while more == 'да':
 
-    computer_card = koloda.pop()
-    computer_points += computer_card
+    computer_card = deck.pop()
+    computer_points += get_card_points(computer_card)
 
     if move == 1:
         your_cards = []
         for i in range(2):
-            your_card = koloda.pop()
+            your_card = deck.pop()
             your_cards.append(your_card)
-            your_points += your_card
+            your_points += get_card_points(your_card)
     else:
-        your_card = koloda.pop()
-        your_points += your_card
+        your_card = deck.pop()
+        your_points += get_card_points(your_card)
+
     print(f'''
     ==============
     ОЧКИ:
@@ -48,7 +79,7 @@ while more == 'да':
 
 else:
     while computer_points < min_computer_point:
-        computer_card = koloda.pop()
+        computer_card = deck.pop()
         computer_points += computer_card
 
         print(f'''
